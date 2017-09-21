50d17647de138cad3cde2c63bf1996bad638b817
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-09-21 10:20:45 -0500

    gets user following

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/getFollowers/:user', function(req, res, next) {
+
+const user = req.params.user;
+console.log(user);
+    
+var GitHubApi = require("github");
+ 
+var github = new GitHubApi({
+
+});
+ 
+// TODO: optional authentication here depending on desired endpoints. See below in README.
+ 
+github.users.getFollowingForUser({
+
+    username: user
+}, function(err, data) {
+    res.json(data)
+});
 });
 
 module.exports = router;
