8d2d35b59d5038952af07ce9b1e88a85e7c23dda
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-09-21 10:25:51 -0500

    added github

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'front/public')));
 
+
+//-------------------------------------------------------
+
 app.use('/', index);
 //app.use('/users', users);
 
